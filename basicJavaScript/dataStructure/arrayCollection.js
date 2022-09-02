900df2714ca141c426f12a76c7de609aca6ea7d3
Katherine Negrete <katherine.negrete.aguilar@gmail.com>
2022-09-02 17:32:55 -0500

    content(code): add javascript file about slice

---
+++ basicJavaScript/dataStructure/arrayCollection.js
@@ -0,0 +1,22 @@
+let complexArray = [
+  [
+    {
+      one: 1,
+      two: 2,
+    },
+    {
+      three: 3,
+      four: 4,
+    },
+  ],
+  [
+    {
+      a: "a",
+      b: "b",
+    },
+    {
+      c: "c",
+      d: "d",
+    },
+  ],
+];
